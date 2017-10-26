88e13930fc88aa380e5ccd11a406e76532a26d95
Rafael Ramirez Lozano <rafaelrl85@hotmail.com>
2017-10-25 23:04:02 -0500

    Bootstrap.yml adding remote configuration in order to retrieve from server
    application.yml, removing words and local configuration

--- src/main/java/com/example/lab4subject/com/example/lab4subject/controller/WordControllerLab4.java
+++ src/main/java/com/example/lab4subject/com/example/lab4subject/controller/WordControllerLab4.java
@@ -1,6 +1,7 @@
 package com.example.lab4subject.com.example.lab4subject.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class WordControllerLab4 {
     @Value("${words}")
     String words;
 
+    @GetMapping("/")
     public @ResponseBody String getWord(){
         String[] wordArray = words.split(",");
         return Arrays.stream(wordArray).findAny().get();
